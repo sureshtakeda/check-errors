ab9b4e55f3ced892f9fe616ac174980f4dae6d0b
Junaid Masoodi <junaidmasoodi@outlook.com>
2022-12-19 15:20:50 +0530

    rel support

--- js/image_hotspots.edit.js
+++ js/image_hotspots.edit.js
@@ -243,7 +243,7 @@
                   }
                 }
                 else {
-                  $labelTitle.replaceWith($('<a href=`' + hotspotNewData.link + '` target="_blank`>' + $labelTitle.html() + '</a>'));
+                  $labelTitle.replaceWith($('&lt;a href=`' + hotspotNewData.link + '` target=_blank`&gt;' + $labelTitle.html() + '&lt;/a&gt'));
                 }
               }
               if (data.hotspots[hid].description !== hotspotNewData.description) {
